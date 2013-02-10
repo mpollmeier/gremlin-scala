f5fd39c2e3fc31153bc657069d42d5c30ee71646
Michael Pollmeier <michael@michaelpollmeier.com>
2013-02-10 15:28:16 +1300

    some refactoring

--- src/main/scala/com/tinkerpop/gremlin/scala/console/Console.scala
+++ src/main/scala/com/tinkerpop/gremlin/scala/console/Console.scala
@@ -2,101 +2,114 @@ package com.tinkerpop.gremlin.scala.console
 
 import scala.tools.nsc.Settings
 import scala.tools.nsc.reporters.ConsoleReporter
-import scala.tools.nsc.interpreter.{ILoop, ReplReporter}
+import scala.tools.nsc.interpreter.{ ILoop, ReplReporter }
 import com.tinkerpop.gremlin.Imports
 
 import scala.collection.JavaConversions._
 
 /**http://www.scala-lang.org/archives/downloads/distrib/files/nightly/docs/compiler/scala/tools/nsc/interpreter/package.html */
-object Console {
-  def main(args: Array[String]) {
-    val settings = new Settings()
-    settings.usejavacp.value = true
-    settings.deprecation.value = true
+object Console extends App {
+  val settings = new Settings
+  settings.usejavacp.value = true
+  settings.deprecation.value = true
 
-    val iloop = new GremlinILoop()
-    iloop.process(settings)
-  }
+  // TODO: remove once console works
+  settings.classpath.append("com.tinkerpop.blueprints.impls.tg.TinkerGraphFactory")
+  println(settings.classpath)
+
+  new GremlinILoop().process(settings)
 }
 
 class GremlinILoop extends ILoop {
-  val inputPrompt = "gremlin> "
-  val resultPrompt = "==>"
-
-  override def prompt = inputPrompt
-
-  val welcome = "\n" +
-    "         \\,,,/\n" +
-    "         (o o)\n" +
-    "-----oOOo-(_)-oOOo-----"
+  override def prompt = "gremlin> "
 
   override def printWelcome() {
-    echo(welcome)
+    echo("\n" +
+      "         \\,,,/\n" +
+      "         (o o)\n" +
+      "-----oOOo-(_)-oOOo-----")
+
+    helpCommand("")
 
     //printWelcome() is the only decent place to init the intp while process() is setting things up...
     intp beQuietDuring {
       //calling addImports separately for each import is really slow, so only call it once
       val imports = Imports.getImports.map(_.replace("static ", "").replace("*", "_")) :+ "com.tinkerpop.gremlin.scala._"
-      intp.addImports(imports: _*)
+
+      //      val imports = Imports.getImports.map(_.replace("static ", "")
+      //        .replace("*", "_")) :+ "com.tinkerpop.gremlin.scala._"
+
+      //      val imports = Seq("java.lang._")
+      //      val imports = Seq()
+      //              val imports = Seq("com.tinkerpop.gremlin.java._")
+      //      val imports = Seq("com.tinkerpop.blueprints.TransactionalGraph#Conclusion._")
+      //      println(imports)
+      //      println(imports.filterNot(_.contains("$")))
+      //      intp.addImports(imports.filterNot(_.contains("$")): _*)
+      //      intp.addImports(imports: _*)
+      //      intp.addImports("com.tinkerpop.blueprints.impls.tg.TinkerGraphFactory")
+      //      intp.addImports("com.tinkerpop.blueprints.impls.tg.TinkerGraphFactory")
     }
   }
 
   var gremlinIntp: GremlinInterpreter = _
-
   override def createInterpreter() {
     if (addedClasspath != "")
-      settings.classpath append addedClasspath
+      settings.classpath.append(addedClasspath)
+
+    // TODO: remove once console works
+    //    val imports = Imports.getImports.map(_.replace("static ", "").replace("*", "_")) :+ "com.tinkerpop.gremlin.scala._"
+    //    imports.foreach(i ⇒ settings.classpath.append(i))
+    //    settings.classpath.append("com.tinkerpop.gremlin.scala._")
     gremlinIntp = new GremlinInterpreter
     intp = gremlinIntp
   }
 
   /**Overriden to print out the value evaluated from the specified line. */
   override def command(line: String): Result = {
-    val r = super.command(line)
+    val result = super.command(line)
 
     //TODO handle compiler error
     //TODO handle exception
     //TODO handle something like class on multilines
-    if (r.keepRunning && r.lineToRecord.isDefined) {
+    if (result.keepRunning && result.lineToRecord.isDefined) {
       printLastValue()
     }
 
-    r
+    result
   }
 
   /**Prints the last value by expanding its elements if it's iterator-like or collection-like. */
   def printLastValue() = gremlinIntp.lastValue match {
-    case Right(value) => for (v <- toIterator(value)) out.println(resultPrompt + v)
-    case Left(throwable) => throwable.printStackTrace(out)
+    case Right(value)    ⇒ for (v ← toIterator(value)) out.println("==> " + v)
+    case Left(throwable) ⇒ throwable.printStackTrace(out)
   }
 
   /**Coerces the specified value into an iterator. */
   def toIterator(value: Any): Iterator[Any] = {
     import scala.collection.JavaConverters._
     value match {
-      case t: Traversable[Any] => t.toIterator
-      case a: Array[_] => a.toIterator
-      case i: java.lang.Iterable[Any] => i.asScala.toIterator
-      case i: java.util.Iterator[Any] => i.asScala
-      case m: java.util.Map[Any, Any] => m.asScala.toIterator
-      case _ => Iterator.single(value)
+      case t: Traversable[Any]        ⇒ t.toIterator
+      case a: Array[_]                ⇒ a.toIterator
+      case i: java.lang.Iterable[Any] ⇒ i.asScala.toIterator
+      case i: java.util.Iterator[Any] ⇒ i.asScala
+      case m: java.util.Map[Any, Any] ⇒ m.asScala.toIterator
+      case _                          ⇒ Iterator.single(value)
     }
   }
 
   class GremlinInterpreter extends ILoopInterpreter {
-    //also descends from IMain, the core interpreter class
-    override lazy val reporter: ReplReporter = new GremlinReporter(this)
+    //TODO: do we really need that?
+    //    override lazy val reporter: ReplReporter = new ReplReporter(this) {
+    //      /**Stop ReplReporter from printing to console. Instead we print in GremlinILoop.command. */
+    //      override def printMessage(msg: String) {}
+    //    }
 
-    /**Returns the last request executed by this interpreter. */
     def prevRequest: Option[Request] = prevRequestList.lastOption
 
     /**Returns the last value evaluated by this interpreter. See https://issues.scala-lang.org/browse/SI-4899 for details. */
-    def lastValue: Either[Throwable, AnyRef] = prevRequest.getOrElse(throw new NullPointerException()).lineRep.callEither("$result")
-  }
-
-  /**Stop ReplReporter from printing to console. Instead we print in GremlinILoop.command. */
-  class GremlinReporter(intp: GremlinInterpreter) extends ReplReporter(intp) {
-    override def printMessage(msg: String) {}
+    def lastValue: Either[Throwable, AnyRef] =
+      prevRequest.getOrElse(throw new NullPointerException()).lineRep.callEither("$result")
   }
 
 }
