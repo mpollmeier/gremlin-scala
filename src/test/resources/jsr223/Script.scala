c9815d4609d90406516329c3620689f50d28c17e
Michael Pollmeier <michael@michaelpollmeier.com>
2013-08-04 21:05:54 +1200

    delete unused file

--- src/test/resources/jsr223/Script.scala
+++
@@ -1,8 +0,0 @@
-
-import com.tinkerpop.gremlin.scala.jsr223.ScalaScript
-import com.tinkerpop.blueprints.impls.tg._
-import scala.collection.mutable
-class Script(vals: mutable.Map[String, Any]) extends ScalaScript { 
-  val a  = vals("a").asInstanceOf[Int]
-          def result =  val a = "dummy response" 
-       }
