d754388d63a354855b6194ecc2f7f2b1226a4146
Michael Pollmeier <michael@michaelpollmeier.com>
2014-02-18 11:42:35 +0000

    path step first steps

--- src/test/scala/com/tinkerpop/gremlin/scala/PathSpec.scala
+++ src/test/scala/com/tinkerpop/gremlin/scala/PathSpec.scala
@@ -4,13 +4,30 @@ import org.scalatest.matchers.ShouldMatchers
 
 class PathSpec extends TestBase {
 
-  //it("works") {
-    //import scala.collection.JavaConversions._
-    //import com.tinkerpop.gremlin.process._
-    //import com.tinkerpop.gremlin.process.steps.util.optimizers.HolderOptimizer
+  it("works") {
+    import scala.collection.JavaConversions._
+    import com.tinkerpop.gremlin.process._
+    import com.tinkerpop.gremlin.process.steps.map._
+    import com.tinkerpop.gremlin.process.steps.util.optimizers.HolderOptimizer
 
-    //doesnt work: simpleholder
+    //works now
     //v(1).out.traversal.path().toList.toList foreach println
+
+    //this works
+    //class MyPathStep[S](traversal: Traversal[_,_]) extends PathStep[S](traversal)
+
+    //doesnt work: SimpleHolder?? anything specific about PathStep?
+    class MyPathStep[S](traversal: Traversal[_,_]) extends MapStep[S, Path](traversal) {
+      this.setFunction { holder: Holder[S] => holder.getPath }
+    }
+
+    // if pathStep is part of the traversal, it works
+    //val t = v(1).out.traversal.path()
+    val t = v(1).out.traversal
+    t.addStep(new MyPathStep[String](t))
+    //HolderOptimizer.doPathTracking(t)
+    //t.toList.toList foreach println
+
 
     //still doesnt work - how can i get the holderoptimizer to be part of the graph?
     //val t0:Traversal[_,_] = graph.v(1).out()
@@ -21,6 +38,6 @@ class PathSpec extends TestBase {
     //new HolderOptimizer().optimize(t1)
     //println(HolderOptimizer.trackPaths(t1))
     //t1.toList.toList foreach println
-  //}
+  }
 
 }
