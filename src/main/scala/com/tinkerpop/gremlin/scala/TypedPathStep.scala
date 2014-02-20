af31371ac3b21c435ebfd61349ccc3e1fe4bc528
Michael Pollmeier <michael@michaelpollmeier.com>
2014-02-20 10:43:24 +0000

    path step works fully statically typed - yui!

---
+++ src/main/scala/com/tinkerpop/gremlin/scala/TypedPathStep.scala
@@ -0,0 +1,23 @@
+package com.tinkerpop.gremlin.scala
+
+import shapeless._
+import ops.hlist._
+import com.tinkerpop.gremlin.process._
+import com.tinkerpop.gremlin.process.steps.map.MapStep
+import com.tinkerpop.gremlin.process.steps.PathConsumer
+
+class TypedPathStep[S, Types <: HList](traversal: Traversal[_,_]) extends MapStep[S, Types](traversal) with PathConsumer {
+
+  this.setFunction { holder: Holder[S] => 
+    toHList(toList(holder.getPath)): Types
+  }
+
+  def toList(path: Path) =
+    (for (i <- 0 until path.size) yield path.get[Any](i)).toList
+
+  private def toHList[T <: HList](path: List[_]): T =
+    if(path.length == 0)
+      HNil.asInstanceOf[T]
+    else
+      (path.head :: toHList[IsHCons[T]#T](path.tail)).asInstanceOf[T]
+}
--- src/test/scala/com/tinkerpop/gremlin/scala/PathSpec.scala
+++ src/test/scala/com/tinkerpop/gremlin/scala/PathSpec.scala
@@ -1,46 +1,22 @@
 package com.tinkerpop.gremlin.scala
 
 import org.scalatest.matchers.ShouldMatchers
+import shapeless._
 
 class PathSpec extends TestBase {
 
-  it("works") {
-    import scala.collection.JavaConversions._
-    import com.tinkerpop.gremlin.process._
-    import com.tinkerpop.gremlin.process.steps.map._
-    import com.tinkerpop.gremlin.process.steps.util.optimizers.HolderOptimizer
+  it("keeps the types in the traversal") {
+    val path: List[Vertex :: Vertex :: String :: HNil] = 
+      v(1).out.value[String]("name").path.toList
 
-    //works now
-    //v(1).out.traversal.path().toList.toList foreach println
-
-    //this works
-    //class MyPathStep[S](traversal: Traversal[_,_]) extends PathStep[S](traversal)
-
-    //doesnt work: SimpleHolder?? anything specific about PathStep?
-    class MyPathStep[S](traversal: Traversal[_,_]) extends MapStep[S, Path](traversal) {
-      this.setFunction { holder: Holder[S] => holder.getPath }
-    }
-
-    // if pathStep is part of the traversal, it works
-    //val t = v(1).out.traversal.path()
-    val t = v(1).out.traversal
-    t.addStep(new MyPathStep[String](t))
-    //HolderOptimizer.doPathTracking(t)
-    //t.toList.toList foreach println
-
-
-    //still doesnt work - how can i get the holderoptimizer to be part of the graph?
-    //val t0:Traversal[_,_] = graph.v(1).out()
-    //val t1: Traversal[_,_] = t0.path()
-    //println(HolderOptimizer.trackPaths(t0))
-    //println(HolderOptimizer.trackPaths(t1))
-    //HolderOptimizer.doPathTracking(t1)
-    //new HolderOptimizer().optimize(t1)
-    //println(HolderOptimizer.trackPaths(t1))
-    //t1.toList.toList foreach println
+    path should be(List(
+      v(1).vertex :: v(3).vertex :: "lop" :: HNil,
+      v(1).vertex :: v(2).vertex :: "vadas" :: HNil,
+      v(1).vertex :: v(4).vertex :: "josh" :: HNil
+    ))
   }
 
   //TODO: also test with steps that don't add to the path:
-  //  order, shuffle, has, filter, dedup, except
-
+  //  order, shuffle, has, filter, dedup, except, as
+  //TODO: back, jump etc.
 }
