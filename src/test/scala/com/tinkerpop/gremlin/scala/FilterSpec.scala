13a01f5b010f1d763d59dd342d83dae757538216
Michael Pollmeier <michael@michaelpollmeier.com>
2014-02-21 11:40:17 +0000

    test for second dedup step

--- src/test/scala/com/tinkerpop/gremlin/scala/FilterSpec.scala
+++ src/test/scala/com/tinkerpop/gremlin/scala/FilterSpec.scala
@@ -5,7 +5,7 @@ import org.scalatest.matchers.ShouldMatchers
 class FilterSpec extends TestBase {
 
   it("filters") {
-    //TODO: always convert to ScalaVertex?
+    //TODO: always convert to ScalaVertex/Edge where possible?
     gs.V
       .filter { v: Vertex => ScalaVertex(v).value("age", default = 0) > 30 }
       .value[String]("name").toSet should be(Set("josh", "peter"))
@@ -13,5 +13,14 @@ class FilterSpec extends TestBase {
 
   it("dedups") {
     v(1).out.in.dedup.toList should be(v(1).out.in.toSet.toList)
+  }
+
+  it("dedups by a given uniqueness function") {
+    v(1).out.in
+      .dedup(v => ScalaVertex(v).property[String]("lang").orElse(null))
+      .value[String]("name").toList should be(List("josh"))
+  }
+
+  it("emits everything but an 'except' list") {
   }
 }
