2b4c0dabeecc28df7dcd7bdfeed5aec31c3b8b5b
Michael Pollmeier <michael@michaelpollmeier.com>
2014-02-28 14:17:49 +0000

    ScalaFilterTest

--- src/test/scala/com/tinkerpop/gremlin/scala/GremlinStandardTestSuite.scala
+++ src/test/scala/com/tinkerpop/gremlin/scala/GremlinStandardTestSuite.scala
@@ -10,24 +10,42 @@ import java.util.{Map => JMap}
 import java.io.File
 import org.apache.commons.configuration.Configuration
 import scala.collection.JavaConversions._
-import com.tinkerpop.gremlin.process.steps.filter.DedupTest
+import com.tinkerpop.gremlin.process.steps.filter._
 import com.tinkerpop.gremlin.process._
 
 object Tests {
   // actual tests are inside an object so that they are not executed twice
   class ScalaDedupTest extends DedupTest with StandardTest {
-    override def get_g_V_both_dedup_name(): Traversal[Vertex, String] = 
-      ScalaGraph(g).V.both.dedup.value[String]("name")
+    override def get_g_V_both_dedup_name = ScalaGraph(g).V.both.dedup.value[String]("name")
 
-    override def get_g_V_both_dedupXlangX_name(): Traversal[Vertex, String] = 
+    override def get_g_V_both_dedupXlangX_name = 
       ScalaGraph(g).V.both
         .dedup(_.getProperty[String]("lang").orElse(null))
         .value[String]("name")
   }
+
+  class ScalaFilterTest extends FilterTest with StandardTest {
+    override def get_g_V_filterXfalseX = ScalaGraph(g).V.filter(_ => false)
+
+    override def get_g_V_filterXtrueX = ScalaGraph(g).V.filter(_ => true)
+
+    override def get_g_V_filterXlang_eq_javaX = ScalaGraph(g).V.filter(_.getProperty("lang").orElse("none") == "java")
+
+    override def get_g_v1_out_filterXage_gt_30X = 
+      ScalaGraph(g).v("1").get.out.filter(_.getProperty("age").orElse(0) > 30)
+
+    override def get_g_V_filterXname_startsWith_m_OR_name_startsWith_pX = ScalaGraph(g).V.filter { v =>
+      val name = v.getValue[String]("name")
+      name.startsWith("m") || name.startsWith("p")
+    }
+  }
 }
 
+import Tests._
 class ScalaProcessStandardSuite(clazz: Class[_], builder: RunnerBuilder) extends AbstractGremlinSuite(clazz, builder, Array(
-    classOf[Tests.ScalaDedupTest]))
+  classOf[ScalaDedupTest],
+  classOf[ScalaFilterTest]
+  ))
 
 trait StandardTest {
   implicit def toTraversal[S,E](gs: GremlinScala[_,E]): Traversal[S,E] = gs.traversal.asInstanceOf[Traversal[S,E]]
