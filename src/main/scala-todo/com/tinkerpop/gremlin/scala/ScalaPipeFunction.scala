31633d83432bc81a9f85f6ca48717cf029131b7a
Michael Pollmeier <michael@michaelpollmeier.com>
2014-05-11 16:29:12 +1200

    drop old stuff from gremlin 2

--- src/main/scala-todo/com/tinkerpop/gremlin/scala/ScalaPipeFunction.scala
+++
@@ -1,8 +0,0 @@
-package com.tinkerpop.gremlin.scala
-
-import com.tinkerpop.gremlin.Tokens
-import com.tinkerpop.pipes.PipeFunction
-
-class ScalaPipeFunction[S, E](fun: S ⇒ E) extends PipeFunction[S, E] {
-  override def compute(s: S) = fun(s)
-}
