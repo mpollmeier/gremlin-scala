1ff6223e0eea43aeeb381a5ee3a0d05f3c73edfe
Michael Pollmeier <michael@michaelpollmeier.com>
2014-06-29 12:27:27 +1200

    update to scala 2.11 - had to throw away jsr and console support for now

--- src/main/scala/com/tinkerpop/gremlin/scala/jsr223/Interpreter.scala
+++ src/main/scala/com/tinkerpop/gremlin/scala/jsr223/Interpreter.scala
@@ -1,24 +1,24 @@
-package com.tinkerpop.gremlin.scala.jsr223
+//package com.tinkerpop.gremlin.scala.jsr223
 
-import scala.collection.JavaConversions._
-import scala.tools.nsc._
-import scala.tools.nsc.interpreter._
-import java.io.PrintWriter
-import scala.tools.nsc.reporters.Reporter
-import com.tinkerpop.gremlin.scala.Imports
+//import scala.collection.JavaConversions._
+//import scala.tools.nsc._
+//import scala.tools.nsc.interpreter._
+//import java.io.PrintWriter
+//import scala.tools.nsc.reporters.Reporter
+//import com.tinkerpop.gremlin.scala.Imports
 
-class Interpreter(out: PrintWriter) extends IMain(new Settings, out) {
-  beQuietDuring {
-    addImports(Imports.asList: _*)
-  }
+//class Interpreter(out: PrintWriter) extends IMain(new Settings, out) {
+  //beQuietDuring {
+    //addImports(Imports.asList: _*)
+  //}
 
-  override lazy val classLoader = new AbstractFileClassLoader(virtualDirectory, getClass.getClassLoader)
+  //override lazy val classLoader = new AbstractFileClassLoader(virtualDirectory, getClass.getClassLoader)
 
-  override protected def newCompiler(settings: Settings, reporter: Reporter) = {
-    settings.outputDirs.setSingleOutput(virtualDirectory)
-    settings.usejavacp.value = true
-    new Global(settings, reporter) with ReplGlobal
-  }
+  //override protected def newCompiler(settings: Settings, reporter: Reporter) = {
+    //settings.outputDirs.setSingleOutput(virtualDirectory)
+    //settings.usejavacp.value = true
+    //new Global(settings, reporter) with ReplGlobal
+  //}
 
-}
+//}
 
--- src/main/scala/com/tinkerpop/gremlin/scala/jsr223/ScriptEngineFactory.scala
+++ src/main/scala/com/tinkerpop/gremlin/scala/jsr223/ScriptEngineFactory.scala
@@ -16,134 +16,134 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package com.tinkerpop.gremlin.scala.jsr223
+//package com.tinkerpop.gremlin.scala.jsr223
 
-import java.io.PrintWriter
-import java.io.Reader
-import java.io.StringWriter
-import java.lang.reflect.InvocationTargetException
-import java.security.PrivilegedActionException
-import java.security.AccessController
-import java.security.PrivilegedAction
-import javax.script.ScriptContext
-import javax.script.{
-  ScriptEngineFactory ⇒ JavaxEngineFactory,
-  Compilable,
-  CompiledScript,
-  ScriptEngine,
-  AbstractScriptEngine,
-  Bindings,
-  SimpleBindings,
-  ScriptException
-}
-import scala.tools.nsc.interpreter._
-import _root_.scala.collection.JavaConversions._
-import com.tinkerpop.gremlin.scala.Gremlin
+//import java.io.PrintWriter
+//import java.io.Reader
+//import java.io.StringWriter
+//import java.lang.reflect.InvocationTargetException
+//import java.security.PrivilegedActionException
+//import java.security.AccessController
+//import java.security.PrivilegedAction
+//import javax.script.ScriptContext
+//import javax.script.{
+  //ScriptEngineFactory ⇒ JavaxEngineFactory,
+  //Compilable,
+  //CompiledScript,
+  //ScriptEngine,
+  //AbstractScriptEngine,
+  //Bindings,
+  //SimpleBindings,
+  //ScriptException
+//}
+//import scala.tools.nsc.interpreter._
+//import _root_.scala.collection.JavaConversions._
+//import com.tinkerpop.gremlin.scala.Gremlin
 
 /**
  * Copied and adapted from clerazza scala script engine: https://github.com/apache/clerezza
  * The code isn't really idiomatic scala, but it works...
  */
-class ScriptEngineFactory() extends JavaxEngineFactory {
-  val interpreter = new Interpreter(new PrintWriter(System.out))
-  val msgWriter = new StringWriter
+//class ScriptEngineFactory() extends JavaxEngineFactory {
+  //val interpreter = new Interpreter(new PrintWriter(System.out))
+  //val msgWriter = new StringWriter
 
-  val name = "gremlin-scala"
-  override def getEngineName() = name
-  override def getEngineVersion() = Gremlin.version
-  override def getExtensions() = List(name)
-  override def getMimeTypes() = List("application/x-scala")
-  override def getNames() = List(name)
-  override def getLanguageName() = name
-  override def getLanguageVersion = "2.10.1"
-  override def getParameter(key: String) = key match {
-    case ScriptEngine.ENGINE           ⇒ getEngineName
-    case ScriptEngine.ENGINE_VERSION   ⇒ getEngineVersion
-    case ScriptEngine.NAME             ⇒ name
-    case ScriptEngine.LANGUAGE         ⇒ getLanguageName
-    case ScriptEngine.LANGUAGE_VERSION ⇒ getLanguageVersion
-    case _                             ⇒ null
-  }
+  //val name = "gremlin-scala"
+  //override def getEngineName() = name
+  //override def getEngineVersion() = Gremlin.version
+  //override def getExtensions() = List(name)
+  //override def getMimeTypes() = List("application/x-scala")
+  //override def getNames() = List(name)
+  //override def getLanguageName() = name
+  //override def getLanguageVersion = "2.10.1"
+  //override def getParameter(key: String) = key match {
+    //case ScriptEngine.ENGINE           ⇒ getEngineName
+    //case ScriptEngine.ENGINE_VERSION   ⇒ getEngineVersion
+    //case ScriptEngine.NAME             ⇒ name
+    //case ScriptEngine.LANGUAGE         ⇒ getLanguageName
+    //case ScriptEngine.LANGUAGE_VERSION ⇒ getLanguageVersion
+    //case _                             ⇒ null
+  //}
 
-  override def getMethodCallSyntax(obj: String, m: String, args: String*) = s"$obj.$m(${args.mkString(",")})"
-  override def getOutputStatement(toDisplay: String) = s"""println("$toDisplay")"""
-  override def getProgram(statements: String*) = statements.mkString("\n")
-  override def getScriptEngine: ScriptEngine = GremlinScalaScriptEngine
+  //override def getMethodCallSyntax(obj: String, m: String, args: String*) = s"$obj.$m(${args.mkString(",")})"
+  //override def getOutputStatement(toDisplay: String) = s"""println("$toDisplay")"""
+  //override def getProgram(statements: String*) = statements.mkString("\n")
+  //override def getScriptEngine: ScriptEngine = GremlinScalaScriptEngine
 
   /**
    * Inner object as it accesse interpreter
    */
-  object GremlinScalaScriptEngine extends AbstractScriptEngine() with Compilable {
-    override def eval(script: Reader, context: ScriptContext): Object = {
-      val scriptStringWriter = new StringWriter()
-      var ch = script.read
-      while (ch != -1) {
-        scriptStringWriter.write(ch)
-        ch = script.read
-      }
-      eval(scriptStringWriter.toString, context)
-    }
+  //object GremlinScalaScriptEngine extends AbstractScriptEngine() with Compilable {
+    //override def eval(script: Reader, context: ScriptContext): Object = {
+      //val scriptStringWriter = new StringWriter()
+      //var ch = script.read
+      //while (ch != -1) {
+        //scriptStringWriter.write(ch)
+        //ch = script.read
+      //}
+      //eval(scriptStringWriter.toString, context)
+    //}
 
-    import ScriptEngineFactory.this.interpreter.Request
-    private def responseLine(request: Request): String = (request.termNames, request.getEval) match {
-      case (name :: _, Some(eval)) ⇒ s"$name: ${request.typeOf(name)} = $eval"
-      case (name :: _, None)       ⇒ s"$name: ${request.typeOf(name)}"
-      case (Nil, _)                ⇒ ""
-    }
+    //import ScriptEngineFactory.this.interpreter.Request
+    //private def responseLine(request: Request): String = (request.termNames, request.getEval) match {
+      //case (name :: _, Some(eval)) ⇒ s"$name: ${request.typeOf(name)} = $eval"
+      //case (name :: _, None)       ⇒ s"$name: ${request.typeOf(name)}"
+      //case (Nil, _)                ⇒ ""
+    //}
 
-    override def eval(script: String, context: ScriptContext): Object = {
-      for (
-        scope ← context.getScopes if (context.getBindings(scope.intValue) != null);
-        (name, obj) ← context.getBindings(scope.intValue)
-      ) interpreter.bind(name, getAccessibleClass(obj.getClass).getName, obj)
+    //override def eval(script: String, context: ScriptContext): Object = {
+      //for (
+        //scope ← context.getScopes if (context.getBindings(scope.intValue) != null);
+        //(name, obj) ← context.getBindings(scope.intValue)
+      //) interpreter.bind(name, getAccessibleClass(obj.getClass).getName, obj)
 
-      val result = interpreter.interpret(script)
-      if (interpreter.reporter.hasErrors) {
-        throw new ScriptException("some error", "script-file", 1)
-      }
+      //val result = interpreter.interpret(script)
+      //if (interpreter.reporter.hasErrors) {
+        //throw new ScriptException("some error", "script-file", 1)
+      //}
 
-      responseLine(interpreter.lastRequest)
-    }
+      //responseLine(interpreter.lastRequest)
+    //}
 
-    override def getFactory() = ScriptEngineFactory.this
-    override def createBindings(): Bindings = new SimpleBindings
+    //override def getFactory() = ScriptEngineFactory.this
+    //override def createBindings(): Bindings = new SimpleBindings
 
-    override def compile(script: Reader): CompiledScript = ???
-    override def compile(script: String): CompiledScript = ???
+    //override def compile(script: Reader): CompiledScript = ???
+    //override def compile(script: String): CompiledScript = ???
 
     /**
      * returns an accessible class or interface that is implemented by class,
      * is doesn't look for superinterfaces of implement interfaces
      */
-    private def getAccessibleClass(clazz: Class[_]): Class[_] =
-      if (isAccessible(clazz)) {
-        clazz
-      } else {
-        val foo: Class[_] = clazz.getInterfaces()(0)
-        for (implementedInterface ← clazz.getInterfaces()) {
-          if (isAccessible(implementedInterface)) return implementedInterface
-        }
-        getAccessibleSuperClass(clazz)
-      }
+    //private def getAccessibleClass(clazz: Class[_]): Class[_] =
+      //if (isAccessible(clazz)) {
+        //clazz
+      //} else {
+        //val foo: Class[_] = clazz.getInterfaces()(0)
+        //for (implementedInterface ← clazz.getInterfaces()) {
+          //if (isAccessible(implementedInterface)) return implementedInterface
+        //}
+        //getAccessibleSuperClass(clazz)
+      //}
 
-    private def getAccessibleSuperClass(clazz: Class[_]): Class[_] = {
-      val superClass = clazz.getSuperclass
-      if (superClass == null) {
-        throw new RuntimeException("No upper class to be checked for accessibility for " + clazz)
-      }
-      if (isAccessible(superClass))
-        superClass
-      else
-        getAccessibleSuperClass(superClass)
+    //private def getAccessibleSuperClass(clazz: Class[_]): Class[_] = {
+      //val superClass = clazz.getSuperclass
+      //if (superClass == null) {
+        //throw new RuntimeException("No upper class to be checked for accessibility for " + clazz)
+      //}
+      //if (isAccessible(superClass))
+        //superClass
+      //else
+        //getAccessibleSuperClass(superClass)
 
-    }
+    //}
 
-    private def isAccessible(clazz: Class[_]) =
-      try {
-        Class.forName(clazz.getName)
-        true
-      } catch {
-        case e: Exception ⇒ false
-      }
-  }
-}
+    //private def isAccessible(clazz: Class[_]) =
+      //try {
+        //Class.forName(clazz.getName)
+        //true
+      //} catch {
+        //case e: Exception ⇒ false
+      //}
+  //}
+//}
