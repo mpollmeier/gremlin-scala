61b5cfce1b3e4a70a9e118fcd65f73ac68205b4d
Michael Pollmeier <michael@michaelpollmeier.com>
2014-08-19 20:12:07 +1200

    drop type

--- src/test/scala/com/tinkerpop/gremlin/scala/TestBase.scala
+++ src/test/scala/com/tinkerpop/gremlin/scala/TestBase.scala
@@ -7,7 +7,7 @@ import org.scalatest.FunSpec
 
 trait TestGraph {
   val graph = TinkerFactory.createClassic()
-  def gs: ScalaGraph = GremlinScala(graph)
+  def gs = GremlinScala(graph)
   def v(i: Int) = gs.v(i:Integer).get
   def e(i: Int) = gs.e(i:Integer).get
 
