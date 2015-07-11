fde6b4e4075508142d1c44e50e4a41fbb7ea4a92
Michael Pollmeier <michael@michaelpollmeier.com>
2015-07-12 06:00:18 +1200

    labelled types don't matter for given and/or traversals

--- src/main/scala/gremlin/scala/GremlinScala.scala
+++ src/main/scala/gremlin/scala/GremlinScala.scala
@@ -353,10 +353,10 @@ object GremlinScala {
 
     def hasNot(key: String, value: Any) = GremlinScala[End, Labels](traversal.where(P.not(__.has[End](key, value))))
 
-    def and(traversals: (GremlinScala[End, HNil] ⇒ GremlinScala[End, HNil])*) =
+    def and(traversals: (GremlinScala[End, _] ⇒ GremlinScala[End, _])*) =
       GremlinScala[End, Labels](traversal.and(traversals.map { _(start).traversal }: _*))
 
-    def or(traversals: (GremlinScala[End, HNil] ⇒ GremlinScala[End, HNil])*) =
+    def or(traversals: (GremlinScala[End, _] ⇒ GremlinScala[End, _])*) =
       GremlinScala[End, Labels](traversal.or(traversals.map { _(start).traversal }: _*))
 
     def local[A](localTraversal: GremlinScala[End, HNil] ⇒ GremlinScala[A, _]) =
