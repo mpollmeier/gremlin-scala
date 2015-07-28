abbe741f20f8fedcebf88217410bbd13557a26fc
Joan <joan@goyeau.com>
2015-07-28 10:29:30 +0100

    Update MarshallingMacros.scala

--- gremlin-scala-macros/src/main/scala/gremlin/scala/MarshallingMacros.scala
+++ gremlin-scala-macros/src/main/scala/gremlin/scala/MarshallingMacros.scala
@@ -1,50 +1,55 @@
 package gremlin.scala
 
 import gremlin.scala.ScalaVertex
-
+import scala.Product
 import scala.language.experimental.macros
 import scala.reflect.ClassTag
 import scala.reflect.api.TypeTags.TypeTag
 import scala.reflect.macros.blackbox.Context
+import scala.reflect.api.Symbols.MethodSymbol
 
 object MarshallingMacros {
+  // Sources:
+  // http://stackoverflow.com/questions/17223213/scala-macros-making-a-map-out-of-fields-of-a-class-in-scala
+  // http://blog.echo.sh/post/65955606729/exploring-scala-macros-map-to-case-class-conversion
 
-  def marshalling(cc: Any): Map[String, Any] = macro marshallingMacro
-
-  def marshallingMacro(c: Context)(cc: c.Tree): c.Tree = {
+  /**
+   * Case Class to Map
+   */
+  def toMap[P <: Product](cc: P): Map[String, Any] = macro toMapMacro(cc)
+  
+  def toMapMacro[P: c.WeakTypeTag](c: Context)(cc: c.Tree) = {
     import c.universe._
-    q"$cc"
+    
+    val toMapParams = weakTypeOf[P].declarations.collect {
+      case field: MethodSymbol if field.isCaseAccessor =>
+        val name = field.name
+        val decoded = name.decoded
+        q"$decoded -> $cc.$name"
+    }
+    
+    q"Map(..$toMapParams)"
   }
 
-  def maximum(a: Int, b: Int): Int =
-    macro maximumMacro
-
-  def maximumMacro(c: Context)(a: c.Tree, b: c.Tree): c.Tree = {
+  /**
+   * Map to Case Class
+   */
+  def fromMap[P <: Product](map: Map[String, Any]): Map[String, Any] = macro fromMapMacro[P](map)
+  
+  def fromMapMacro[P: c.WeakTypeTag](c: Context)(map: c.Tree) = {
     import c.universe._
-    q"if($a > $b) $a else $b"
-  }
-
-  def betterMaximum(a: Double, b: Double): Double =
-    macro betterMaximumMacro
-
-  def betterMaximumMacro(c: Context)(a: c.Tree, b: c.Tree): c.Tree = {
-    import c.universe._
-
-    // We ask scalac to generate fresh names to avoid potential naming conflicts:
-    //
-    // See the following (step 10 onwards) for a discussion of name generation:
-    //     https://github.com/scalamacros/macrology201/commits/part1
-    //
-    // Note that this will fail for certain exotic types of arguments
-    // due to an owner chain corruption issue. See steps 19 to 23 above
-    // for a detailed explanation and workaround.
-    val temp1 = c.freshName(TermName("temp"))
-    val temp2 = c.freshName(TermName("temp"))
-
-    q"""
-     val $temp1 = $a
-     val $temp2 = $b
-     if($temp1 > $temp2) $temp1 else $temp2
-     """
+    
+    val tpe = weakTypeOf[P]
+    val companion = tpe.typeSymbol.companionSymbol
+    
+    val fromMapParams = tpe.declarations.collect {
+      case field: MethodSymbol if field.isCaseAccessor =>
+        val name = field.name
+        val decoded = name.decoded
+        val returnType = tpe.declaration(name).typeSignature
+        q"map($decoded).asInstanceOf[$returnType]"
+    }
+    
+    q"$companion(..$fromMapParams)"
   }
 }
