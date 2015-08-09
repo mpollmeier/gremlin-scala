d138a30cd540c3ef285fb7fda2b4fc730713c022
Daniel Krieg <daniel_krieg@mac.com>
2015-08-09 11:24:59 -0500

    Added syntax for creating bidirectional edges.

--- src/main/scala/gremlin/scala/SemiEdge.scala
+++ src/main/scala/gremlin/scala/SemiEdge.scala
@@ -2,4 +2,11 @@ package gremlin.scala
 
 case class SemiEdge(from: Vertex, label: String, properties: Map[String, Any]) {
   def ->(to: Vertex) = from.addEdge(label, to).setProperties(properties)
+
+  def <->(to: Vertex) = {
+    val e0: Edge = from.addEdge(label, to).setProperties(properties)
+    val e1: Edge = to.addEdge(label, from).setProperties(properties)
+//    (e0, e1)
+    to.graph().E(e0, e1)
+  }
 }
--- src/test/scala/gremlin/scala/ArrowSyntaxSpec.scala
+++ src/test/scala/gremlin/scala/ArrowSyntaxSpec.scala
@@ -3,6 +3,7 @@ package gremlin.scala
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph
 import org.scalatest.FunSpec
 import org.scalatest.Matchers
+import shapeless.HNil
 
 class ArrowSyntaxSpec extends FunSpec with Matchers {
 
@@ -30,5 +31,44 @@ class ArrowSyntaxSpec extends FunSpec with Matchers {
     e.outVertex shouldBe paris
     e.value("type") shouldBe Some("WDiEdge")
     e.value("weight") shouldBe Some(2)
+  }
+
+  it("should support bidirectional connections") {
+    val graph: Graph = TinkerGraph.open
+
+    val jupiter = graph.addVertex("jupiter")
+    val neptune = graph.addVertex("neptune")
+
+    val gs: GremlinScala[Edge, HNil] = jupiter -- "brother" <-> neptune
+    val edges = gs.toList()
+    edges.size shouldBe 2
+
+    val e0: ScalaEdge = edges.head
+    val e1: ScalaEdge = edges.tail.head
+
+    e0.inVertex shouldBe neptune
+    e1.inVertex shouldBe jupiter
+
+    e0.outVertex shouldBe jupiter
+    e1.outVertex shouldBe neptune
+  }
+
+  it("should support bidirectional connections with properties") {
+    val graph: Graph = TinkerGraph.open
+
+    val jupiter = graph.addVertex("jupiter")
+    val neptune = graph.addVertex("neptune")
+
+    val gs: GremlinScala[Edge, HNil] = jupiter -- ("brother", "type" -> "WDiEdge", "weight" -> 2) <-> neptune
+    val edges = gs.toList()
+
+    val e0: ScalaEdge = edges.head
+    val e1: ScalaEdge = edges.tail.head
+
+    e0.value("type") shouldBe Some("WDiEdge")
+    e0.value("weight") shouldBe Some(2)
+
+    e1.value("type") shouldBe Some("WDiEdge")
+    e1.value("weight") shouldBe Some(2)
   }
 }
