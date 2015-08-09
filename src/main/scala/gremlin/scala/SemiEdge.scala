fd5d05c4f046532f3094104c08ba5a26c7916710
Daniel Krieg <daniel_krieg@mac.com>
2015-08-08 23:47:45 -0500

    Added support for passing varargs of pairs to set the properties of the Edge after adding it.

--- src/main/scala/gremlin/scala/SemiEdge.scala
+++ src/main/scala/gremlin/scala/SemiEdge.scala
@@ -1,5 +1,5 @@
 package gremlin.scala
 
-case class SemiEdge(from: Vertex, label: String) {
-  def ->(to: Vertex) = from.addEdge(label, to)
+case class SemiEdge(from: Vertex, label: String, properties: Map[String, Any]) {
+  def ->(to: Vertex) = from.addEdge(label, to).setProperties(properties)
 }
--- src/test/scala/gremlin/scala/ArrowSyntaxSpec.scala
+++ src/test/scala/gremlin/scala/ArrowSyntaxSpec.scala
@@ -17,4 +17,18 @@ class ArrowSyntaxSpec extends FunSpec with Matchers {
     e.inVertex shouldBe london
     e.outVertex shouldBe paris
   }
+
+  it("add edge with properties using syntax sugar") {
+    val graph: Graph = TinkerGraph.open
+
+    val paris = graph.addVertex("Paris")
+    val london = graph.addVertex("London")
+
+    val e = paris -- ("eurostar", "type" -> "WDiEdge", "weight" -> 2) -> london
+
+    e.inVertex shouldBe london
+    e.outVertex shouldBe paris
+    e.value("type") shouldBe Some("WDiEdge")
+    e.value("weight") shouldBe Some(2)
+  }
 }
