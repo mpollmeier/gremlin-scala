d944ddc79588a58eed2502a7cac0016f55a5fc81
Daniel Krieg <daniel_krieg@mac.com>
2015-08-09 11:29:10 -0500

    Added syntax for creating bidirectional edges.

--- src/main/scala/gremlin/scala/SemiEdge.scala
+++ src/main/scala/gremlin/scala/SemiEdge.scala
@@ -6,7 +6,6 @@ case class SemiEdge(from: Vertex, label: String, properties: Map[String, Any]) {
   def <->(to: Vertex) = {
     val e0: Edge = from.addEdge(label, to).setProperties(properties)
     val e1: Edge = to.addEdge(label, from).setProperties(properties)
-//    (e0, e1)
     to.graph().E(e0, e1)
   }
 }
