6562e0ec88e2f2c321805bb306059729cb1d591e
Michael Pollmeier <michael@michaelpollmeier.com>
2015-10-14 18:04:14 +1300

    fix SchemaSpec

--- gremlin-scala/src/test/scala/gremlin/scala/SchemaSpec.scala
+++ gremlin-scala/src/test/scala/gremlin/scala/SchemaSpec.scala
@@ -87,8 +87,8 @@ class SchemaSpec extends FunSpec with Matchers {
 
       e.inVertex shouldBe london
       e.outVertex shouldBe paris
-      e.value[String](Type.key) shouldBe Some("WDiEdge")
-      e.value[String](Weight.key) shouldBe Some(2)
+      e.value[String](Type.key) shouldBe "WDiEdge"
+      e.value[Integer](Weight.key) shouldBe 2
     }
 
     it("to add left edge using syntax sugar with just Label") {
