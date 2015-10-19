8037791770015f5c3be6ed3725317083ee85b60e
Michael Pollmeier <michael@michaelpollmeier.com>
2015-10-20 09:40:04 +1300

    remove ID

--- gremlin-scala/src/main/scala/gremlin/scala/schema.scala
+++ gremlin-scala/src/main/scala/gremlin/scala/schema.scala
@@ -15,8 +15,6 @@ object schema {
 
   case class Key[A](override val key: String) extends Atom[A](key)
 
-  object ID extends Atom[Any](T.id.name)
-
   implicit class AtomValue[A](p: (String, A)) {
     def key: String = p._1
     def value: A = p._2
