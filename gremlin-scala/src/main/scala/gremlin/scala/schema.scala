5ab6f479ac1ab9e41143876e77dd812422ed9848
Michael Pollmeier <michael@michaelpollmeier.com>
2015-10-20 09:45:15 +1300

    remove atom - one test failure

--- gremlin-scala/src/main/scala/gremlin/scala/schema.scala
+++ gremlin-scala/src/main/scala/gremlin/scala/schema.scala
@@ -1,22 +1,23 @@
 package gremlin.scala
 
-import org.apache.tinkerpop.gremlin.structure._
+// import org.apache.tinkerpop.gremlin.structure._
 
-import scala.language.implicitConversions
+// import scala.language.implicitConversions
 
 object schema {
   /**
     * Smaller than an Element, consisting of the particles used
     * compose a Property (or VertexProperty) of an Element (Edge or Vertex)
     */
-  sealed abstract class Atom[A](val key: String) {
-    def apply(n: A): (String, A) = key → n
-  }
+  // sealed abstract class Atom[A](val key: String) {
+  //   def apply(n: A): (String, A) = key → n
+  // }
 
-  case class Key[A](override val key: String) extends Atom[A](key)
+  case class Key[A](key: String)
+  // case class Key[A](override val key: String) extends Atom[A](key)
 
-  implicit class AtomValue[A](p: (String, A)) {
-    def key: String = p._1
-    def value: A = p._2
-  }
+  // implicit class AtomValue[A](p: (String, A)) {
+  //   def key: String = p._1
+  //   def value: A = p._2
+  // }
 }
