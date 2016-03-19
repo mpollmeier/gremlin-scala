ac389bcf21714b6c9a5514c7663961116b1f541f
Michael Pollmeier <michael@michaelpollmeier.com>
2016-03-19 15:52:54 +1300

    where(Traversal) should take => GremlinScala[Boolean, _]

--- gremlin-scala/src/main/scala/gremlin/scala/GremlinScala.scala
+++ gremlin-scala/src/main/scala/gremlin/scala/GremlinScala.scala
@@ -394,7 +394,7 @@ case class GremlinScala[End, Labels <: HList](traversal: GraphTraversal[_, End])
 
   def where(startKey: String, predicate: P[String]) = GremlinScala[End, Labels](traversal.where(startKey, predicate))
 
-  def where(whereTraversal: GremlinScala[End, HNil] ⇒ GremlinScala[_, _]) =
+  def where(whereTraversal: GremlinScala[End, HNil] ⇒ GremlinScala[Boolean, _]) =
     GremlinScala[End, Labels](traversal.where(whereTraversal(start).traversal))
 
   def addV() = GremlinScala[Vertex, Labels](traversal.addV())
