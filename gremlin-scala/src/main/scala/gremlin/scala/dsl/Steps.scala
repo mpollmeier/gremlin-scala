66dcf626272e1f0914865ef056ef83d78ec3300c
Michael Pollmeier <michael@michaelpollmeier.com>
2017-05-11 10:43:47 +1200

    add more steps to dsl: hasId, filterOnEnd, aggregate

--- gremlin-scala/src/main/scala/gremlin/scala/dsl/Steps.scala
+++ gremlin-scala/src/main/scala/gremlin/scala/dsl/Steps.scala
@@ -2,6 +2,7 @@ package gremlin.scala.dsl
 
 import gremlin.scala._
 import shapeless._
+import scala.collection.mutable
 
 /** root type for all domain types */
 trait DomainRoot extends Product {
@@ -26,6 +27,12 @@ class Steps[EndDomain, EndGraph](val raw: GremlinScala[EndGraph, HNil])(
   def head(): EndDomain = converter.toDomain(raw.head)
   def headOption(): Option[EndDomain] = raw.headOption.map(converter.toDomain)
 
+  def hasId[NewSteps](id: AnyRef)(
+    implicit
+    isElement: EndGraph <:< Element,
+    constr: Constructor.Aux[EndDomain, EndGraph, NewSteps]): NewSteps =
+    constr(raw.hasId(id))
+
   def map[NewEndDomain, NewEndGraph, NewSteps <: StepsRoot](fun: EndDomain ⇒ NewEndDomain)(
     implicit
     newConverter: Converter.Aux[NewEndDomain, NewEndGraph],
@@ -48,14 +55,51 @@ class Steps[EndDomain, EndGraph](val raw: GremlinScala[EndGraph, HNil])(
           // not sure why I need the cast here - should be safe though
         }
       }
+
 }
 
 /* Root class for all your vertex based DSL steps
- * basically just a shortcut for getting the implicits/types right
  * TODO: add support for as/select - currently always HNil
  * TODO: add support for using Edge instead of Vertex?
  */
 class NodeSteps[EndDomain <: DomainRoot](override val raw: GremlinScala[Vertex, HNil])(
   implicit marshaller: Marshallable[EndDomain]) extends Steps[EndDomain, Vertex](raw)(
-  Converter.forDomainNode[EndDomain](marshaller, raw.traversal.asAdmin.getGraph.get)
-)
+  Converter.forDomainNode[EndDomain](marshaller, raw.traversal.asAdmin.getGraph.get)) {
+
+  /** Aggregate all objects at this point into the given collection, e.g. `mutable.ArrayBuffer.empty[EndDomain]`
+    * Uses eager evaluation (as opposed to `store`() which lazily fills a collection)
+    */
+  def aggregate[NewSteps](into: mutable.Buffer[EndDomain])(
+    implicit constr: Constructor.Aux[EndDomain, Vertex, NewSteps]): NewSteps =
+    constr(
+      raw.sideEffect{ v: Vertex =>
+        into += v.toCC[EndDomain]
+      }
+    )
+
+  // def filter(predicate: Self => Steps[_]): Self = {
+  //   val rawWithFilter: GremlinScala[EndGraph, HNil] =
+  //     /* TODO: remove cast */
+  //     raw.filter{ gs => predicate(construct(gs.asInstanceOf[GremlinScala[Vertex, HNil]])).raw }
+  //   construct(rawWithFilter)
+  // }
+
+  def filterOnEnd[NewSteps](predicate: EndDomain => Boolean)(
+    implicit constr: Constructor.Aux[EndDomain, Vertex, NewSteps]): NewSteps =
+    constr(
+      raw.filterOnEnd { v: Vertex =>
+        predicate(v.toCC[EndDomain])
+      }
+    )
+
+  // def or(traversals: (Self => Steps[_])*) : Self = {
+  //   val foo = traversals.map(
+  //     trav => { gs : GremlinScala[Vertex, HNil] => trav(construct(gs)).raw } )
+  //   construct(raw.or(foo :_*))
+  // }
+
+  // def filterRaw(predicate: GremlinScala[EndGraph, _] => GremlinScala[_, _]): Self =
+  //   construct(raw.filter(predicate))
+
+  // def dedup(): Self = construct(raw.dedup())
+}
--- gremlin-scala/src/test/scala/gremlin/scala/dsl/DslSpec.scala
+++ gremlin-scala/src/test/scala/gremlin/scala/dsl/DslSpec.scala
@@ -3,6 +3,7 @@ package gremlin.scala.dsl
 import gremlin.scala._
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerFactory
 import org.scalatest.{WordSpec, Matchers}
+import scala.collection.mutable
 import shapeless._
 
 class DslSpec extends WordSpec with Matchers {
@@ -10,7 +11,7 @@ class DslSpec extends WordSpec with Matchers {
 
   "finds all persons" in {
     val graph = TinkerFactory.createModern
-    val personSteps = new PersonSteps(graph.V.hasLabel[Person])
+    val personSteps = PersonSteps(graph)
     personSteps.toSet shouldBe Set(
       Person(Some(1), "marko", 29),
       Person(Some(2), "vadas", 27),
@@ -21,14 +22,14 @@ class DslSpec extends WordSpec with Matchers {
 
   "traverses from person to software" in {
     val graph = TinkerFactory.createModern
-    val personSteps = new PersonSteps(graph.V(1))
+    val personSteps = PersonSteps(graph).hasId(1: Integer)
     personSteps.created.toSet shouldBe Set(Software("lop", "java"))
   }
 
   "finds combination of person/software in for comprehension" in {
     implicit val graph = TinkerFactory.createModern
     val traversal = for {
-      person   <- new PersonSteps(graph.V.hasLabel[Person])
+      person   <- PersonSteps(graph)
       software <- person.created
     } yield (person.name, software)
 
@@ -39,12 +40,30 @@ class DslSpec extends WordSpec with Matchers {
       ("josh", Software("ripple", "java"))
     )
   }
+
+  "aggregate intermediary results into a collection" in {
+    val graph = TinkerFactory.createModern
+    val allPersons = mutable.ArrayBuffer.empty[Person]
+
+    val markos: List[Person] =
+      PersonSteps(graph)
+        .aggregate(allPersons)
+        .filterOnEnd(_.name == "marko")
+        .toList()
+
+    markos.size shouldBe 1
+    allPersons.size should be > 1
+  }
+
 }
 
 object TestDomain {
   @label("person") case class Person(@id id: Option[Integer], name: String, age: Integer) extends DomainRoot
   @label("software") case class Software(name: String, lang: String) extends DomainRoot
 
+  object PersonSteps {
+    def apply(graph: Graph) = new PersonSteps(graph.V.hasLabel[Person])
+  }
   class PersonSteps(override val raw: GremlinScala[Vertex, HNil]) extends NodeSteps[Person](raw) {
     def created() = new SoftwareSteps(raw.out("created"))
   }
