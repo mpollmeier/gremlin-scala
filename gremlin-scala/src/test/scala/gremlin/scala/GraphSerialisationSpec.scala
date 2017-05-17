9d5382a98bfa523e0a9205c2eac723a017be1512
Michael Pollmeier <michael@michaelpollmeier.com>
2017-05-17 17:02:31 +1200

    graphson v2

--- gremlin-scala/src/test/scala/gremlin/scala/GraphSerialisationSpec.scala
+++ gremlin-scala/src/test/scala/gremlin/scala/GraphSerialisationSpec.scala
@@ -1,7 +1,8 @@
 package gremlin.scala
 
-import org.apache.tinkerpop.gremlin.structure.io.Io
+import java.io.FileOutputStream
 import org.apache.tinkerpop.gremlin.structure.io.IoCore
+import org.apache.tinkerpop.gremlin.structure.io.graphson.GraphSONVersion
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.{TinkerFactory, TinkerGraph}
 import org.scalatest.{Matchers, WordSpec}
 
@@ -20,8 +21,19 @@ class GraphSerialisationSpec extends WordSpec with Matchers {
     }
 
     "support graphson" in {
-      val file = "target/tinkerpop-modern.graphson"
+      val file = "target/tinkerpop-modern.graphson.json"
       graph.io(IoCore.graphson).writeGraph(file)
+
+      val newGraph = TinkerGraph.open
+      newGraph.io(IoCore.graphson).readGraph(file)
+      newGraph.V.count.head shouldBe 6
+      newGraph.E.count.head shouldBe 6
+    }
+
+    "support graphson v2" in {
+      val file = "target/tinkerpop-modern.graphson2.json"
+      val mapper = graph.io(IoCore.graphson).mapper.normalize(true).version(GraphSONVersion.V2_0).create
+      graph.io(IoCore.graphson).writer.mapper(mapper).create.writeGraph(new FileOutputStream(file), graph)
 
       val newGraph = TinkerGraph.open
       newGraph.io(IoCore.graphson).readGraph(file)
