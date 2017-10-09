249baaee25afd2b0b5a539f2b6280ac255a6c684
Michael Pollmeier <michael@michaelpollmeier.com>
2017-10-10 11:13:45 +1300

    more ways to create Ps

--- gremlin-scala/src/main/scala/gremlin/scala/P.scala
+++ gremlin-scala/src/main/scala/gremlin/scala/P.scala
@@ -7,6 +7,8 @@ import scala.collection.JavaConverters._
 /** the scala version of tinkerpop's P, mostly to avoid unnecessarily complicated constructs
   * like P.within(vertices.asJava: JCollection[Vertex]) */
 object P {
+  // same as `eq`, in case you're having problems with overloaded definition of `eq`
+  def is[A](value: A) = JavaP.eq(value)
   def eq[A](value: A) = JavaP.eq(value)
   def neq[A](value: A) = JavaP.neq(value)
   def gt[A](value: A) = JavaP.gt(value)
@@ -18,4 +20,6 @@ object P {
   def outside[A](a1: A, a2: A) = JavaP.outside(a1, a2)
   def within[A](iterable: Iterable[A]) = JavaP.within(iterable.asJavaCollection: JCollection[A])
   def without[A](iterable: Iterable[A]) = JavaP.without(iterable.asJavaCollection: JCollection[A])
+
+  def fromPredicate[A](predicate: (A, A) => Boolean, value: A) = new JavaP(toJavaBiPredicate(predicate), value)
 }
