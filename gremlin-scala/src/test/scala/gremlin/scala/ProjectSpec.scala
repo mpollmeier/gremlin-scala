8d47c49d534b2aec871ceb4735aff8ec9a269a66
Michael Pollmeier <michael@michaelpollmeier.com>
2019-02-28 06:47:25 +1300

    remove non-sensical test

--- gremlin-scala/src/test/scala/gremlin/scala/ProjectSpec.scala
+++ gremlin-scala/src/test/scala/gremlin/scala/ProjectSpec.scala
@@ -5,19 +5,6 @@ import org.scalatest.{Matchers, WordSpec}
 
 class ProjectSpec extends WordSpec with Matchers {
 
-  "projecting by one thing" in {
-    // val result: String = //fails
-    // val result: (String) = //fails
-    val result: Tuple1[String] =
-      // val result =
-      graph.V
-        .has(name.of("marko"))
-        .project(_(By(name)))
-        .head
-    println(result) //(marko)
-    println(result.getClass) // scala.Tuple1
-  }
-
   "projecting by two traversals" in {
     val result: (java.lang.Long, java.lang.Long) =
       graph.V
