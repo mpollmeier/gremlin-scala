cdcdbf88db8b6393f32de64efa0adeb8cf066b47
Kevin Wright <kev.lee.wright@gmail.com>
2022-08-30 15:34:02 +0100

    Remove deprecated usage of IoCore

--- gremlin-scala/src/test/scala/gremlin/scala/GraphSerialisationSpec.scala
+++ gremlin-scala/src/test/scala/gremlin/scala/GraphSerialisationSpec.scala
@@ -1,8 +1,9 @@
 package gremlin.scala
 
 import java.io.FileOutputStream
-import org.apache.tinkerpop.gremlin.structure.io.IoCore
-import org.apache.tinkerpop.gremlin.structure.io.graphson.GraphSONVersion
+import org.apache.tinkerpop.gremlin.structure.io.graphml.GraphMLIo
+import org.apache.tinkerpop.gremlin.structure.io.graphson.{GraphSONIo, GraphSONVersion}
+import org.apache.tinkerpop.gremlin.structure.io.gryo.GryoIo
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.{TinkerFactory, TinkerGraph}
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
@@ -13,20 +14,20 @@ class GraphSerialisationSpec extends AnyWordSpec with Matchers {
 
     "support graphML" in {
       val file = "target/tinkerpop-modern.graphml"
-      graph.io(IoCore.graphml).writeGraph(file)
+      graph.io(GraphMLIo.build()).writeGraph(file)
 
       val newGraph = TinkerGraph.open
-      newGraph.io(IoCore.graphml).readGraph(file)
+      newGraph.io(GraphMLIo.build()).readGraph(file)
       newGraph.V().count().head() shouldBe 6
       newGraph.E().count().head() shouldBe 6
     }
 
     "support graphson" in {
       val file = "target/tinkerpop-modern.graphson.json"
-      graph.io(IoCore.graphson).writeGraph(file)
+      graph.io(GraphSONIo.build()).writeGraph(file)
 
       val newGraph = TinkerGraph.open
-      newGraph.io(IoCore.graphson).readGraph(file)
+      newGraph.io(GraphSONIo.build()).readGraph(file)
       newGraph.V().count().head() shouldBe 6
       newGraph.E().count().head() shouldBe 6
     }
@@ -34,34 +35,34 @@ class GraphSerialisationSpec extends AnyWordSpec with Matchers {
     "support graphson v2" in {
       val file = "target/tinkerpop-modern.graphson2.json"
       val mapper = graph
-        .io(IoCore.graphson)
+        .io(GraphSONIo.build())
         .mapper
         .normalize(true)
         .version(GraphSONVersion.V2_0)
         .create
       graph
-        .io(IoCore.graphson)
+        .io(GraphSONIo.build())
         .writer
         .mapper(mapper)
         .create
         .writeGraph(new FileOutputStream(file), graph)
 
       val newGraph = TinkerGraph.open
-      newGraph.io(IoCore.graphson).readGraph(file)
+      newGraph.io(GraphSONIo.build()).readGraph(file)
       newGraph.V().count().head() shouldBe 6
       newGraph.E().count().head() shouldBe 6
     }
 
     "support gryo/kryo" in {
       val file = "target/tinkerpop-modern.gryo"
-      graph.io(IoCore.gryo).writeGraph(file)
+      graph.io(GryoIo.build()).writeGraph(file)
 
       val newGraph = TinkerGraph.open
-      newGraph.io(IoCore.gryo).readGraph(file)
+      newGraph.io(GryoIo.build()).readGraph(file)
       newGraph.V().count().head() shouldBe 6
       newGraph.E().count().head() shouldBe 6
     }
   }
 
-  def graph = TinkerFactory.createModern
+  def graph: TinkerGraph = TinkerFactory.createModern
 }
